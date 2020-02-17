eefa586908a40a994df2e932baac607e31cece09
HAINES-DESKTOP\richa <richardkhaines@hotmail.co.uk>
2020-02-17 19:58:32 +0100

    none

--- packages/gatsby-theme-vulcan/gatsby-node.js
+++ packages/gatsby-theme-vulcan/gatsby-node.js
@@ -19,6 +19,7 @@ exports.createPages = ({ actions, graphql }) => {
             }
           categories {
             title
+            isRoot
           }
           defaultProductVariant {
             sku
